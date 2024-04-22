2012eb0ad173d0eea52482367d8f8ae690df768f
Aayan <128874715+MUIabk@users.noreply.github.com>
2024-04-22 22:57:47 +0530

    Update B_Tree_Exploration.cpp

--- B_Tree_Exploration.cpp
+++ B_Tree_Exploration.cpp
@@ -1,318 +1,222 @@
-/*
-" If you set your goals ridiculously high, even if its a failure
-You will fail above everyone else's success" 
-*/
+// by Siddhid Saha (2112010)
 
-#pragma GCC optimize("O1")
-#pragma GCC optimize("O2")
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-#pragma GCC optimize("O3,unroll-loops")
+
 #include <bits/stdc++.h>
 using namespace std;
 
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-using namespace __gnu_cxx;
-typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ind_set ;
-typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> ind_mset ;
-typedef map < int , tree < int , null_type , less < int > , rb_tree_tag , tree_order_statistics_node_update  > > ind_map;
-
-typedef unsigned long long ull;
-typedef long double ld;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<vi> vii;
-typedef vector<pii> vpii;
-typedef pair<ll, ll> pll;
-typedef vector<ll> vl;
-typedef vector<pll> vpll;
-typedef vector<vl> vll;
-template<class T> using maxpq = priority_queue<T>;
-template<class T> using minpq = priority_queue<T, vector<T>, greater<T>>;
-template<class T> using vec1 = vector<T>;
-template<class T> using vec2 = vector<vector<T>>;
-template<class T> using vec3 = vector<vector<vector<T>>>;
-
-#define FOR(i, a, b) for (ll i = (a); i < (b); i++)
-#define REP(i,b,a)  for(ll i =((b)-1);i>=(a);i--)
-#define mem(a,x)   memset(a,x,sizeof(a))
+#define INF 1e18
+#define endl "\n"
 #define pb push_back
-#define pf push_front
-#define ff first
-#define ss second
-#define sz(x) ((int)(x).size())
-#define all(v) v.begin(), v.end()
-#define rall(v) (v).rbegin(),(v).rend()
-#define eb emplace_back
+#define ppb pop_back
 #define mp make_pair
-#define mt make_tuple
-#define fbo(a) find_by_order(a) //will give a-th largest element
-#define ook(a) order_of_key(a) //will give no. of elements strictly lesser than a
-#define sz(x) ((int)(x).size())
-#define nzl(x) __builtin_clzll(x)
-#define nzr(x) __builtin_ctzll(x)
-#define setbits(x) __builtin_popcountll(x)
-#define setbitsParity(x) __builtin_parityll(x) // 1 -> odd else 0 if even
-#define umap unordered_map
-#define uset unordered_set
-#define nl "\n"
 #define PI atan(1)*4
-#define E 2.71828
-#define yes {cout << "Yes" << endl; return;}
-#define no {cout << "No" << endl; return;}
-#define YES {cout << "Yes" << endl;}
-#define NO {cout << "No" << endl;}
-#define nyet {cout<<"-1"<<endl;return;}
-#define mxe(v)  (*max_element(v.begin(),v.end()))
-#define mne(v)  (*min_element(v.begin(),v.end()))
-#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
-#define ub upper_bound
-#define lb lower_bound
-#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
-#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
-#define UNIQUE(x) \
-  sort(all(x)), x.erase(unique(all(x)), x.end()), x.shrink_to_fit()
-#define outt(a) \
-      FOR(i,1,sz(a))            \
-      cout << a[i] << " "; \
-      cout << endl;
-#define inn(a) \
-      FOR(i,1,sz(a))            \
-      cin>>a[i];
-#define FAST_AF_BOI                \
-    ios_base ::sync_with_stdio(0); \
-    cin.tie(0);               \
-    cout.tie(0);
+#define set_bits __builtin_popcountllO
+#define all(x) (x).begin(), (x).end()
+#define vi vector<int>
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define rvsort(a) sort(all(a),greater<int>())
+#define read(a,n) for(int i = 0 ; i < n ; i ++){ cin >> a[i];}
+#define printv(a) for(auto it: a){cout << it << " ";} cout << endl;
+#define ms(arr, v) memset(arr, v, sizeof(arr))
 
-struct custom_hash {
-  static uint64_t splitmix64(uint64_t x) {
-      x += 0x9e3779b97f4a7c15;//abk
-      x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-      x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-      return x ^ (x >> 31);
-  }
-  size_t operator()(uint64_t x) const {
-      static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-      return splitmix64(x + FIXED_RANDOM);
-  }
-};
-typedef gp_hash_table<long long,long long,custom_hash> fast_map;
-typedef unordered_map<long long,long long,custom_hash> safe_map;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
 
-// ================================== i/o module ==================================
-template <class T> void _print(T x){cerr<<x;};
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-template<class T>void read(T &x){x=0;int f=0;char ch=getchar();while(ch<'0' || ch>'9')f|=(ch=='-'),ch=getchar();while(ch>='0' && ch<='9')x=(x<<3)+(x<<1)+(ch^48),ch=getchar();x=f? -x:x;return ;}
-template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
-template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
-template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
-template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
-template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
-template <class T> inline vector<T>& operator--(vector<T>& v) { for(auto &x:v) --x; return v; }
-template <class T> inline vector<T>& operator++(vector<T>& v) { for(auto &x:v) ++x; return v; }
-template <class T> inline vector<T>& operator^=(vector<T>& v,int y) { for(auto &x:v) x^=y; return v; }
-inline string& operator^=(string& s,int y) { for(auto &x:s)x=((x-'0')^y)+'0' ; return s; }
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-void dgb_out () { cerr << endl; }
-template < typename Head, typename... Tail >
-void dgb_out ( Head H, Tail... T) { cerr <<' ' << H; dgb_out (T...); }
 #ifndef ONLINE_JUDGE
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dgb_out(__VA_ARGS__) 
+#include "/Users/templates/debug.h"
 #else
-#define dbg(...)
+#define dbg(x...)
 #endif
-
-// ================================================================================
-
-//`````````````````````````````````````````````IMP FUNCTIONS``````````````````````````````````````````````````````
-ll ceil(ll a,ll b){return (a+b-1)/b;}
-int log_2(ull i){return i?nzl(1)-nzl(i):-1;}
+/*---------------------------------------------------------------------------------------------------------------------------*/
 ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
-ll bin_expo(ll a, ll b, ll mod) {ll res = 1;a%=mod;if(a==0)return 0;while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
-ll bin_mul(ll a, ll b, ll mod) {ll res = 0; while (b > 0) {if (b & 1)res = (res + a) % mod; a = (a + a) % mod; b = b >> 1;} return res;}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
 void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
 ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
-ll mminvprime(ll a, ll b) {return bin_expo(a, b - 2, b);}
-ll ncr(ll n, ll r, ll m, ll *fact, ll *ifact) {if(n<r)return 0;ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r];if(n < r) return 0;if(n == r || r == 0) return 1;if(r<0) return 0; return (((val1 * val2) % m) * val3) % m;}
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
 void google(int t) {cout << "Case #" << t << ": ";}
-vl sieve(int n) {int*arr = new int[n + 1](); vl vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
-ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
-ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
-ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
-ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
-ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i*i <= n; i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
-ll large_expo(ll a,ll b,ll c,ll m) {return bin_expo(a,bin_expo(b,c,phin(m)),m);} //(a^b^c)%M 
-ll large_expo_prime(ll a,ll b,ll c,ll m) {return bin_expo(a,bin_expo(b,c,m-1),m);} //(a^b^c)%M when m is prime
-template<class T>vector<T> prefixSum(vector<T> v, bool flag){vector<T> ans;T sum = 0;if (flag){for (auto &e : v){sum += e;ans.eb(sum);}}else{ans.pb(0);REP(i, v.size(), 0){sum += v[i];ans.eb(sum);}reverse(all(ans));}return ans;}
-ll ffs(ll n){if(n==0)return -1;return log2(n & -n);}
-template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
-template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
-mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
 ll uid(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);} 
-//````````````````````````````````````````````````````````````````````````````````````````````````````````````
+/*--------------------------------------------------------------------------------------------------------------------------*/
+//const int mod = 1e9 + 7;
+//const int mod = 998244353; 
 
-const ll INF=1e18+10;
-const ll inf=1e9+10;
-const ll N=1e5+10;
-const ll mod1=(1e9+7);
-const ll mod2=(998244353);
 
-/*
-Self notes cause I need it :
-1. dumbfk read the question properly ... I m fed up with this lack of presence of mind
-2. dont assume that something is given in question ,like the input is sorted or stuff
-   if some step needs the input to be sorted , either check question or sort , dont assume
-3. in case you have less time to debug : 
-    Common errors :
-      -> Wrote wrong variable names at places cause ur stupid	
-     -> For debugging u hard coded small values and submitted that solution right away
-*/
 
-#define int long long
-const int L=18;
-int n,a[N];
-vec2<int>adj(N);
-int lift[N][L],dpth[N],dp[N][L];
-
-void dfs(int u,int p){
-    lift[u][0]=p;
-    dp[u][0]=a[u];
-    for(auto &v:adj[u]){
-        if(v==p)continue;
-        dpth[v]=dpth[u]+1;
-        dfs(v,u);
+int n, l;
+vector<vector<int>> adj;
+vll value;
+struct LCA{
+    vector<vector<int>> g;
+    vector<vector<int>> up;
+    vll tin, tout, depth, sumv;
+    int timer = 0;
+    inline void init(int n){
+        n++;
+        g.resize(n);
+        tin.resize(n);
+        tout.resize(n);
+        depth.resize(n);
+        sumv.resize(n);
+        up.assign(n, vector<int>(31, -1));
     }
-}
+    LCA(){};
+    LCA(int n){
+        init(n);
+    }
 
-void Compute(){
-    dfs(1,0);
-    FOR(j,1,L)
-        FOR(i,1,n+1){
-            lift[i][j]=lift[lift[i][j-1]][j-1];
-            dp[i][j]=dp[i][j-1]+dp[lift[i][j-1]][j-1];
+    inline void add_edge(int u, int v){
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    inline void dfs(int v, int p , int dis, ll val){
+        tin[v] = ++timer;
+        depth[v] = dis;
+        val += value[v];
+        sumv[v] = val; 
+        up[v][0] = p;
+        for (int i = 1; i < 31; i++){
+            up[v][i] = up[up[v][i - 1]][i - 1];
         }
-}
 
-int kthanc(int x,int d){
-    FOR(i,0,L)
-        if(d>>i&1)x=lift[x][i];
-    return x;
-}
-
-int lca(int u,int v){
-    if(dpth[u]>dpth[v])swap(u,v);
-    v=kthanc(v,dpth[v]-dpth[u]);
-    if(u==v)return u;
-    REP(i,L,0)
-        if(lift[v][i]!=lift[u][i])u=lift[u][i],v=lift[v][i];
-    return lift[u][0];
-}
-
-int dist(int u,int v){
-    return dpth[u]+dpth[v]-2*dpth[lca(u,v)];
-}
-
-pll walk(int x,int d){
-    int ans=0;
-    int p=0;
-    FOR(i,0,L){
-        if(d>>i&1){
-            ans+=dp[x][i];
-            x=lift[x][i];
+        for (int u : g[v]){
+            if (u != p){
+                dfs(u, v , dis+1, val);
+            }
         }
-    }return {ans,x};
-}
+        tout[v] = ++timer;
+    }
 
-void transcendent(int tc)
+    void dfs(int root = 1){
+        dfs(root, root , 0, 0);
+    }
+
+    inline bool upper(int u, int v){
+        return tin[u] <= tin[v] && tout[v] <= tout[u];
+    }
+
+    int lca(int u, int v){
+        if (upper(u, v)) return u;
+        if (upper(v, u)) return v;
+        for (int i = 30 ; i >= 0; i--){
+            if (!upper(up[u][i], v)) u = up[u][i];
+        }
+        return up[u][0];
+    }
+
+    inline int dist(int u, int v){
+        return depth[u] + depth[v] - 2 * depth[lca(u, v)];
+    }
+    ll valuv(ll u, ll v){
+        return sumv[u] + sumv[v] - 2*sumv[lca(u, v)]+value[lca(u, v)];
+    }
+
+};
+
+void solve()
 {
-    cin>>n;
-    FOR(i,0,n-1){
-        int u,v;cin>>u>>v;
-        adj[u].pb(v);
-        adj[v].pb(u);
+    cin >> n;
+    assert(n >= 1 && n <= 1e5);
+    adj.resize(n+1);
+    LCA lc(n);
+    for(int i = 0 ; i < n-1 ; i ++){
+        ll u, v; cin >> u >> v;
+        assert(u != v && u >= 1 && u <= n && v >= 1 && v <= n);
+        lc.add_edge(u, v);
     }
-    FOR(i,1,n+1)cin>>a[i];
-    Compute();
-    int q;cin>>q;
-    FOR(_,0,q){        
-        int u,v;cin>>u>>v;
-        int d=dist(u,v),l=lca(u,v);
-        int lo=0,hi=d;
-        int d1=dist(u,l),d2=dist(v,l);
-        auto check=[&](int mid){
-          if(mid==d)return true;
-          int left=(mid+1)*(mid+2)/2,right=(d-mid)*(d-mid+1)/2;
-          if(mid<=d1){
-            auto [val,p]=walk(u,mid);
-            left+=val+a[p];
-          }else{
-            auto [val,p]=walk(u,d1);
-            assert(p==l);
-            left+=val+a[l];
-            int par=kthanc(v,d2-mid+d1);
-            auto [val1,p1]=walk(par,mid-d1);
-            assert(p1==l);
-            left+=val1;
-          }
-          int need=d-mid-1;
-          if(need<=d2){
-            auto [val,p]=walk(v,need);
-            right+=val+a[p];
-          }else{
-            auto [val,p]=walk(v,d2);
-            assert(p==l);
-            right+=val+a[l];
-            int par=kthanc(u,d1-need+d2);
-            auto [val1,p1]=walk(par,need-d2);
-            assert(p1==l);
-            right+=val1;
-          }
-          return left>right;
-        };
-        while(hi-lo>1){
-          int mid=hi+lo>>1;
-          if(check(mid))hi=mid;else lo=mid+1;
-        }if(check(lo))hi=lo;
-        cout<<d+1-hi<<nl;
+    value.resize(n+1);
+    for(int i = 1 ; i<= n ; i ++) cin >> value[i];
+    for(int i = 1 ; i <= n ; i ++){
+        assert(value[i] >= 0 && value[i] <= 1e9);
     }
+    lc.dfs();
+    ll q; cin >> q;
+    assert(q >= 1 && q <= 1e5);
+    while(q--){
+        ll u, v; cin >> u >> v;
+        assert(u != v && u >= 1 && u <= n && v >= 1 && v <= n);
+        ll lca = lc.lca(u ,v);
+        ll d = lc.dist(u, v), du = lc.dist(u, lca);
+        ll l = 0, r = d;
+        while(l < r){
+            ll mid = (l+r)/2;
+            auto check = [&](ll mid) mutable {
+                if(mid > du){
+                    ll x = v;
+                    ll up = d-mid-1;
+                    for(int i = 30 ; i >= 0 ; i--){
+                        if(up&(1LL<<i)){
+                            x = lc.up[x][i];
+                        }
+                    }
+                    ll vv = lc.valuv(v, x);
+                    ll totv = lc.valuv(u, v);
+                    ll remv = totv-vv;
+                    ll totnum = d+1, numv = mid+1;
+                    ll remnum = totnum-numv;
+                    vv += ((remnum)*(remnum+1))/2;
+                    remv += ((numv)*(numv+1))/2;
+                    return remv > vv;
+                }else{
+                    ll x = u;
+                    ll up = mid;
+                    for(int i = 30 ; i >= 0 ; i--){
+                        if(up&(1LL<<i)){
+                            x = lc.up[x][i];
+                        }
+                    }
+                    ll vv = lc.valuv(u, x);
+                    ll totv = lc.valuv(u, v);
+                    ll remv = totv-vv;
+                    // dbg(u, x);
+                    // dbg(vv, remv);
+                    ll totnum = d+1, numv = mid+1;
+                    ll remnum = totnum-numv;
+                    vv += ((numv)*(numv+1))/2;
+                    remv += ((remnum)*(remnum+1))/2;
+                    return vv > remv;
+                }
+            };
+            if(check(mid)){
+                r = mid;
+            }else{
+                l = mid+1;
+            }
+        }
+        // dbg(d, l);
+        ll hh = d-l+1;
+        cout << hh << endl;
+    }
+
+
+
 }
 
-static void read(){
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
+int main() {
+
+ios::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+
+
+ll t = 1;
+// cin >> t;
+for(int i = 1 ; i <= t ; i++){
+//google(i);
+solve();
+}
+return 0;
 }
 
-int32_t main()
-{
-    //read();
-    FAST_AF_BOI
-         auto begin = std::chrono::high_resolution_clock::now();
-    //cout << fixed << setprecision(12);
-    //cerr << fixed << setprecision(0);
-    //clock_t timer;
-    //timer = clock();
-    //PreComp();
-    int test=1;
-    // cin >> test;
-    FOR(tc,1,test+1)
-    {
-            //cerr<<endl<<"----Test:"<<tc<<"----"<<endl;
-            transcendent(tc);
-    }
-         auto end = std::chrono::high_resolution_clock::now();
-         auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-         //cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n"; 
-    return 0;
-}
+
+
+
+
+
+
+
